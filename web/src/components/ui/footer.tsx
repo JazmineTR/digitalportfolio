615146115f2407a5fe9f8dd68479412802fdc952
JazmineTR <jazminetrinh@yahoo.com>
2026-03-05 03:30:45 -0500

    LinkedIn in footer

--- web/src/components/ui/footer.tsx
+++ web/src/components/ui/footer.tsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
+import { LinkedInLogoIcon } from '@radix-ui/react-icons';
+
 const Footer = () => {
   return (
-    <div className="flex justify-center bg-black text-white font-mono py-10">&copy; 2026 jazmine le</div>
+    <div className="flex justify-center items-center gap-4 bg-black text-white font-mono py-10">
+      
+      &copy; 2026 jazmine le
+      <Link to="https://www.linkedin.com/in/jazmine-le-t/">
+                        <LinkedInLogoIcon width={40} height={40} className="border-2 hover:border-indigo-800" color="white" />
+                    </Link>
+      </div>
   );
 };
 
